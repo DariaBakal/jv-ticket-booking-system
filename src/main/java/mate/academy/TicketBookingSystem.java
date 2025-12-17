3ff6ffadadea7ffe8ca7532f6ef454e92455ce1f
DariaBakal <bakal.dasha01@gmail.com>
2025-12-17 12:34:16 +0200

    implemented TicketBookingSystem class

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,26 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 public class TicketBookingSystem {
+    private  final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            if (semaphore.tryAcquire(100, TimeUnit.MILLISECONDS)) {
+                return new BookingResult(user,
+                        true, "Booking successful.");
+            } else {
+                return new BookingResult(user, false, "No seats available.");
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
